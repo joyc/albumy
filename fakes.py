c0d9779a5d073963d0fe60cf168bb391f670177b
joyc <joyc@live.jp>
2019-05-20 23:36:48 +0900

    init project

---
+++ fakes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*- 
+""" 
+@author: hython 
+@license: MIT 
+@file: fakes.py 
+@time: 2019/05/20
+@contact: hython4@gmail.com
+@site:  
+@software: PyCharm 
+"""  
---
+++ models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*- 
+""" 
+@author: hython 
+@license: MIT 
+@file: models.py 
+@time: 2019/05/20
+@contact: hython4@gmail.com
+@site:  
+@software: PyCharm 
+"""  
