060b228d7ac1b3527d0101787657b7010a10c9bf
joyc <joyc@live.jp>
2019-05-21 00:42:14 +0900

    update project

--- albumy/blueprints/main.py
+++ albumy/blueprints/main.py
@@ -7,4 +7,17 @@
 @contact: hython4@gmail.com
 @site:  
 @software: PyCharm 
-"""  
+"""
+from flask import render_template, Blueprint
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/')
+def index():
+    return render_template('main/index.html')
+
+
+@main_bp.route('/explore')
+def explore():
+    return render_template('main/explore.html')
--- albumy/extensions.py
+++ albumy/extensions.py
@@ -7,4 +7,14 @@
 @contact: hython4@gmail.com
 @site:  
 @software: PyCharm 
-"""  
+"""
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+moment = Moment()
