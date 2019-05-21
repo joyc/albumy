9f6debce64d312cba347ab01c59fb080a6255992
joyc <joyc@live.jp>
2019-05-22 00:41:11 +0900

    add user form

--- albumy/forms/auth.py
+++ albumy/forms/auth.py
@@ -32,3 +32,24 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 128), EqualTo('password2')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField()
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('The email is already in use.')
+
+    def  validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('The username is already in use.')
+
+
+class ForgetPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 254), Email()])
+    submit = SubmitField()
+
+
+class ResetPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 254), Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(8, 128), EqualTo('password2')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField()
