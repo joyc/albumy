c56b9256a6e7b5b6df4727d718ad43695b1cc656
joyc <joyc@live.jp>
2019-05-22 00:26:34 +0900

    add user model

---
+++ albumy/forms/auth.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- 
+""" 
+@author: hython 
+@license: MIT 
+@file: auth.py 
+@time: 2019/05/22
+@contact: hython4@gmail.com
+@site:  
+@software: PyCharm 
+"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
+
+from albumy.models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 254), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Log in')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 254), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20),
+                                                   Regexp('^[a-zA-Z0-9]*$',
+                                                          message='The username should contain only a-z, A-Z and 0-9.')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 128), EqualTo('password2')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField()
